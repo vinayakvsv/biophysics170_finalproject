e0d320fdc183643d8ba913d772b6520b25b15aa9
Vinay Viswanadham <vinayak.vsv@gmail.com>
2017-11-29 17:25:10 -0500

    replicated Rao plot

--- test2.py
+++ test2.py
@@ -6,3 +6,56 @@
 
 print("hello world")
 
+
+# In[13]:
+
+
+#See https://github.com/mirnylab/cooler-binder/blob/master/cooler_api.ipynb for tutorial
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+import h5py
+import sys,os,re
+
+import cooler
+
+
+# In[3]:
+
+
+get_ipython().magic('matplotlib inline')
+
+
+# In[ ]:
+
+
+get_ipython().system('curl -O ftp://cooler.csail.mit.edu/coolers/hg19/Rao2014-GM12878-MboI-allreps-filtered.5kb.cool')
+get_ipython().system('mv Rao2014-GM12878-MboI-allreps-filtered.5kb.cool ./hic_data/')
+
+
+# In[5]:
+
+
+filepath = './hic_data/Rao2014-GM12878-MboI-allreps-filtered.5kb.cool'
+
+
+# In[6]:
+
+
+c = cooler.Cooler(filepath)
+print(c.info)
+print(c.chroms())
+
+
+# In[14]:
+
+
+mat = c.matrix(balance=False, sparse=True)[1000:1200, 1000:1200]
+mat
+arr = mat.toarray()
+arr
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+im = ax.matshow(np.log10(arr), cmap='YlOrRd')
+fig.colorbar(im)
+
